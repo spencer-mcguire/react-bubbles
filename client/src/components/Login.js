406b7b849e1eeb6d65c66fdbfdfc395a2041b2c7
spencer-mcguire <smcguire1826@gmail.com>
2019-12-20 11:06:29 -0600

    login authentication

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+import Loader from "react-loader-spinner";
+
+const initialState = {
+  username: "",
+  password: ""
+};
+const Login = props => {
+  const [creds, setCreds] = useState(initialState);
+  const [loading, setLoading] = useState(false);
+
+  // Form Changes
+  const handleChange = e => {
+    setCreds({
+      ...creds,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  // Form Submit
+  const handleSubmit = e => {
+    e.preventDefault();
+    setLoading(true);
+    axiosWithAuth()
+      .post("/login", creds)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/bubbles");
+      })
+      .catch(err => console.log(err));
+  };
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          value={creds.username}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={creds.password}
+          onChange={handleChange}
+        />
+        <button>Login</button>
+      </form>
+      {loading && (
+        <Loader type="BallTriangle" color="red" height={80} width={80} />
+      )}
     </>
   );
 };
