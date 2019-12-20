ce58579898ece1b590f1283a204daca0e69e30c2
spencer-mcguire <smcguire1826@gmail.com>
2019-12-20 12:14:34 -0600

    add form styles

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -36,21 +36,28 @@ const Login = props => {
     <>
       <h1>Welcome to the Bubble App!</h1>
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="username"
-          value={creds.username}
-          onChange={handleChange}
-          placeholder="username"
-        />
-        <input
-          type="password"
-          name="password"
-          value={creds.password}
-          onChange={handleChange}
-          placeholder="password"
-        />
-        <button>Login</button>
+        <legend>Log In</legend>
+        <label>
+          Username
+          <input
+            type="text"
+            name="username"
+            value={creds.username}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            name="password"
+            value={creds.password}
+            onChange={handleChange}
+          />
+        </label>
+        <div className="button-row">
+          <button>Login</button>
+        </div>
       </form>
       {loading && (
         <Loader type="BallTriangle" color="red" height={80} width={80} />
